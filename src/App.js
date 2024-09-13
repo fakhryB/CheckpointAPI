f79ba6e32541651f2106086ad9ad9ac5659acd98
fakhryB <fakhryprojet@gmail.com>
2024-09-13 14:11:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ListUsers from './Components/ListUsers';
+import NavUser from './Components/NavUser';
+
+function App() {
+  return (
+    <div>
+      <NavUser/>
+      <ListUsers/>
+    </div>
+  );
+}
+
+export default App;
